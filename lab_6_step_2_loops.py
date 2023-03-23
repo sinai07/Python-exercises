25d8016feef0799d55ae7877ed4f4096cae2c972
sina river <eguisina07@gmail.com>
2023-03-23 17:11:01 +0000

    lab_6_step_2_loops

--- lab_6_step_2_loops.py
+++ lab_6_step_2_loops.py
@@ -40,3 +40,25 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
+    # Create a list of the input text
+def new_input_text_list():
+    input_text = open_input()
+    new_list = []
+    for item in input_text:
+        text = item['Text']
+        new_list.append(text)
+    print(new_list)
+    
+    def new_list_comprehension():
+        input_text = open_input()
+        list_comprehension = [item['Text'] for item in input_text]
+        print(list_comprehension)
+
+    
+    def main():
+        new_input_text_list()
+        translate_loop()
+        new_list_comprehension()
+
+    
